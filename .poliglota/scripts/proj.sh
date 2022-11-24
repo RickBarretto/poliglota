6882a2676d2f589ca02c45686fff88338ea200d3
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2022-11-24 15:18:17 -0300

    add proj command

---
+++ .poliglota/scripts/proj.sh
@@ -0,0 +1,217 @@
+#! /bin/bash
+
+## Configuration
+set -C          # Prevent overwriting of files by redirection
+
+E_BADARGS=85    ## Bad Arguments error value
+
+# Internal functions ---
+
+description() {
+    echo "  proj                          project related tasks               "
+    echo "      new <proj-name>           inits a new project with all        "
+    echo "                                avaliable implementations           "
+    echo "      add <impl> <project>      adds a particular implementation to "
+    echo "                                an existing project                 "
+}
+
+usage() {
+    echo "command proj                                                        "
+    echo "poli proj [OPTIONS]... [SUBCOMMANDS]                                "
+    echo
+    echo "SUBCOMMANDS                                                         "
+    echo
+    echo "  new <proj-name>               inits a new project with all        "
+    echo "                                avaliable implementations           "
+    echo "    --custom|-c <script-path>   run with a custom script instead    "
+    echo "    --empty|-e                  creates a empty project             "
+    echo "    --repo|-r <folder>          modifies the output folder          "
+    echo "    --templ|-t <folder>         modifies the template entry folder  "
+    echo
+    echo "  add <impl> <project>          adds a particular implementation to "
+    echo "                                an existing project                 "
+    echo "    --as|-a <new-impl-name>     adds the implementation with a      "
+    echo "                                specific name.                      "
+    echo "    --custom|-c <script-path>   run with a custom script instead    "
+    echo "    --empty|-e                  creates an empty implementation     "
+    echo "    --repo|-r <folder>          modifies the output folder          "
+    echo "    --templ|-t <folder>         modifies the template entry folder  "
+    echo
+    echo "OPTIONS                                                             "
+    echo "  --help|-h                     shows this help page                "
+}
+
+## Creates a new project base on .templates/
+## $1: <project> -> the new project name
+## --custom|-c <script-path>
+## --empty|-e
+## --repo|-o|-r <folder>
+## --templ|-t <folder>
+new_project() {
+
+    # Param checker
+    if [[ ! -n "$1" ]]; then
+        echo "Wrong Parameters"
+        usage
+        exit $E_BADARGS
+    fi
+
+    # local variables
+    local project=""
+    local custom=""
+    local empty=0
+    local repo="repository"
+    local templ=".templates"
+
+    while  [[ -n "$1" ]]; do
+        case $1 in
+            "--custom" | "-c")
+                shift; ./$1 $@
+                exit;;
+            "--empty" | "-e")
+                local empty=1
+                shift;;
+            "--repo" | "-r")
+                local repo="$2"
+                shift 2;;
+            "--templ" | "-t")
+                local templ="$2";
+                shift 2;;
+            *)
+                local project="$1"
+                shift;;
+        esac
+    done
+
+    if [[ -n "$project" ]]; then
+
+        # Just creates a project
+        if [[ $empty == 1 ]]; then
+            mkdir $repo/$project
+            exit
+
+        # Creates the project based on a template
+        else
+            if mkdir $repo/$project; then
+                cp ./$templ/* $repo/$project/ \
+                    -r -b --no-preserve=timestamp
+
+                if [[ "$templ" == ".templates" ]]; then
+                    rm rm -R --force $repo/$project/.template/**
+                fi
+            else echo "This Project already exists"
+            fi; exit
+        fi
+
+    fi
+    exit
+
+}
+
+## Adds an implementation to given project
+## $1: <implementation> -> the name of a existing implementation
+## $2: <project>  -> the name of existing project
+## --as <new-name>
+## --custom|-c <script-path>
+## --empty|-e
+## --repo|-o|-r <folder>
+## --templ|-t <folder>
+add_implementation() {
+
+    # Param checker
+    if [[ ! -n "$2" ]]; then
+        echo "Wrong Parameters"
+        usage
+        exit $E_BADARGS
+    fi
+
+    # local variables
+    local implementation=""
+    local name=""
+    local project=""
+    local repo="repository"
+    local templ=".templates"
+    local empty=0
+
+    while  [[ -n "$1" ]]; do
+        case $1 in
+            "--custom" | "-c")
+                shift; ./$1 $@
+                exit;;
+
+            "--as" | "-a")
+                shift; local name="$1";
+                shift;;
+
+            "--empty" | "-e")
+                local empty=1
+                shift;;
+
+            "--repo" | "-r")
+                shift; local repo="$1"
+                shift;;
+
+            "--templ" | "-t")
+                shift; local templ="$1";
+                shift;;
+
+            *)
+                local implementation="$1"
+                local project="$2"
+                shift 2;;
+        esac
+    done
+
+    if [[ -n "$implementation" && -n "$project" ]]; then
+
+        # Defines $name
+        if [[ -z "$name" ]]; then
+            local name="$implementation"
+        fi
+
+        # Just creates a project
+        if [[ -n "$empty" ]]; then
+            mkdir $repo/$project/$name
+            exit
+
+        # Creates the project based on a implementation's template
+        else
+            mkdir $repo/$project/$name
+            cp $templ/$implementation $repo/$project/$name \
+                -r -b --no-preserve=timestamp
+            exit
+        fi
+    fi; exit
+
+}
+
+# Code execution
+if [[ "$1" == "--description" ]]; then
+    description
+    exit
+elif [[ "$1" == "--help" || "$1" == "-h" ]]; then
+    usage
+    exit
+fi
+
+if [[ ! -n "$1" ]]; then
+    echo "Wrong Parameters"
+    usage
+    exit $E_BADARGS
+fi
+
+case $1 in
+    new)
+        shift;
+        new_project $@
+        exit;;
+    add)
+        shift;
+        add_implementation $@
+        exit;;
+    *)
+        echo "Wrong command"
+        usage
+        exit $E_BADARGS
+    ;;
+esac
