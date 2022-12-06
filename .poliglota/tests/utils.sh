f4aa3cce159840731d6a8191467da3865d0b61a9
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2022-12-06 12:39:08 -0300

    add utils' variables

---
+++ .poliglota/tests/utils.sh
@@ -0,0 +1,30 @@
+## Utils functions and variables to be used
+## by every test function inside this folder
+
+# Use source ".poliglota/tests/utils.sh"
+
+# --- Global variables to be exported ---
+
+## Imports:
+## >> Constants
+##  $STD_CONFIG_PATH
+##  $STD_POLI_PATH
+##  $STD_REPO_PATH
+##  $STD_TEMPL_PATH
+##  $STD_PLUGINS_PATH
+## >> Variables
+##  $LAST_PROJECT
+# shellcheck source=poli.config
+source "poli.config"
+
+## >> Variable
+## This is the current test that is running
+CURRENT_TEST=""
+
+
+## >> Constant
+## String used to be printed in fail(),
+## case the command returned an error
+declare -r PLEASE_DEBUG="Command returned an error.
+Please run with set -x. And read ${STD_POLI_PATH}/error.txt"
+
