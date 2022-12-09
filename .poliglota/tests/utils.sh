e76a6ea760d6943bb1f91aa12be5576f664a31ea
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2022-12-08 21:23:55 -0300

    Add colors to test output

--- .poliglota/tests/utils.sh
+++ .poliglota/tests/utils.sh
@@ -44,7 +44,19 @@ pass() {
     # -- Globals
     local current_test="${CURRENT_TEST}"
 
-    echo "[PASSED] ${current_test}: ${message}"
+    # -- Colors
+    local -r RESET='\033[0m'
+    local -r GREEN='\033[0;32m'
+    local -r GREEN_BG='\033[42m'
+    local -r GREEN_UNDERLINE='\033[4;32m'
+
+    # -- To print
+    local -r status="${GREEN_BG}[PASSED]${RESET}"
+    local -r title="${GREEN_UNDERLINE}${current_test}"
+    local -r divisor="${GREEN}:${RESET}"
+    local -r description="${RESET}${message}"
+
+    echo -e "${status} ${title}${divisor}" "${description}"
 }
 
 
@@ -63,6 +75,18 @@ fail() {
     # -- Globals
     local current_test="${CURRENT_TEST}"
 
-    echo -e "[FAILED] ${current_test}: ${message}" \
+    # -- Colors
+    local -r RESET='\033[0m'
+    local -r RED='\033[0;31m'
+    local -r RED_BG='\033[41m'
+    local -r RED_UNDERLINE='\033[4;31m'
+
+    # -- To print
+    local -r status="${RED_BG}[FAILED]${RESET}"
+    local -r title="${RED_UNDERLINE}${current_test}"
+    local -r divisor="${RED}:${RESET}"
+    local -r description="${RESET}${message}"
+
+    echo -e "${status} ${title}${divisor}" "${description}" \
         >> /dev/stderr
 }
