390ce7bd5bfef347dede6dcdb210435a6026cda8
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2022-12-10 22:06:12 -0300

    add echo_init_tests function

--- .poliglota/tests/utils.sh
+++ .poliglota/tests/utils.sh
@@ -29,6 +29,29 @@ Please run with set -x. And read ${STD_POLI_PATH}/error.txt"
 
 # --- Global functions to be exported ---
 
+## Prints an init messaga
+## Arguments:
+##  $file
+echo_init_tests() {
+    # -- Arguments
+    local -r file="$1"
+
+    # -- Colors
+    local -r RESET='\033[0m'
+    local -r CYAN='\033[0;36m'
+    local -r CYAN_BG='\033[46m'
+    local -r CYAN_UNDERLINE='\033[4;36m'
+
+    # -- To print
+    local -r status="${CYAN_BG}[RUNNING]${RESET}"
+    local -r title="${CYAN_UNDERLINE}${file}${RESET}"
+    local -r divisor="${CYAN}:${RESET}"
+
+    echo -e "\n${status} ${title}${divisor}\n"
+
+}
+
+
 ## Prints a success message for some test
 ## Arguments:
 ##  $message *
