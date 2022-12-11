9caf809c65fb9a925a25a73fe8fc3d61d9504c14
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2022-12-10 21:01:10 -0300

    add generate_template function

--- .poliglota/tests/test_add_proj.sh
+++ .poliglota/tests/test_add_proj.sh
@@ -50,6 +50,32 @@ generate_project() {
 }
 
 
+# TODO: replace by the real template command
+#   It should run with `templ new ...`
+generate_template() {
+    # .poli templ new "$@"    \
+    # 1> /dev/null            \
+    # 2> /dev/stderr          \
+    # || raise_cannot_execute
+
+    local -r template_folder="$1"
+    local -r implementation="$2"
+
+    mkdir "${template_folder}/${implementation}" ||
+        ( echo "Couldn't create directory"              \
+            >> /dev/stderr                              \
+            && raise_cannot_execute )
+
+    cp -r -b --no-preserve=timestamp                    \
+            "${template_folder}/.template/"**           \
+            "${template_folder}/${implementation}/" ||
+        ( echo "Couldn't copy base template"            \
+            >> /dev/stderr                              \
+            && raise_cannot_execute )
+
+}
+
+
 # Testing functions ---------
 
 
